39f2b7c3f7f49c5f5bc106b09db638994e19d01b
George Tegos <george.tegos.22@gmail.com>
2023-07-27 09:19:42 +0100

    Fixed gloves_list position

---
+++ week_04/day_3/glove_shop_app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user@localhost:5432/glove_shop_app"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from controllers.shop_controller import gloves_blueprint
+app.register_blueprint(gloves_blueprint)
