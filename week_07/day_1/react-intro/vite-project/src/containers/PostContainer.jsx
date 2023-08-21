fd9f3f38089109256c9dc990af25fc3cce0d0319
George Tegos <george.tegos.22@gmail.com>
2023-08-21 19:54:32 +0100

    end of w7d1

--- week_07/day_1/react-intro/vite-project/src/containers/PostContainer.jsx
+++ week_07/day_1/react-intro/vite-project/src/containers/PostContainer.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import Header from "../components/Header";
+import PostForm from "../components/PostForm";
+import PostList from "../components/PostList";
 
 function PostContainer(){
-    return (
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "What is a function?",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repellendus autem fuga sapiente nulla aut natus",
+    },
+  ]);
+  
+  return (
         <>
             <Header />
+            <PostForm posts={posts} setPosts={setPosts}/>
+            <PostList posts={posts}/>
         </>
         )
 }
