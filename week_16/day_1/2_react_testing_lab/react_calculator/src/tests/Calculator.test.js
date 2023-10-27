7950fef2aad5ed34550694bb54fed9cda27c3513
George Tegos <george.tegos.22@gmail.com>
2023-10-27 20:26:41 +0100

    unit testing

--- week_16/day_1/2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
+++ week_16/day_1/2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
@@ -54,4 +54,65 @@ describe("Calculator", () => {
     fireEvent.click(equalsOperator);
     expect(runningTotal.textContent).toEqual("15");
   });
+
+  it("should change the running total to 3", () => {
+    const button2 = container.getByTestId("number2");
+    const button1 = container.getByTestId("number1");
+    const button7 = container.getByTestId("number7");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button2);
+    fireEvent.click(button1);
+    const divideOperator = container.getByTestId("operator-divide");
+    const equalsOperator = container.getByTestId("operator-equals");
+    fireEvent.click(divideOperator);
+    fireEvent.click(button7);
+    fireEvent.click(equalsOperator);
+    expect(runningTotal.textContent).toEqual("3");
+  });
+
+  it("should change the running total to 12345", () => {
+    const button1 = container.getByTestId("number1");
+    const button2 = container.getByTestId("number2");
+    const button3 = container.getByTestId("number3");
+    const button4 = container.getByTestId("number4");
+    const button5 = container.getByTestId("number5");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button1);
+    fireEvent.click(button2);
+    fireEvent.click(button3);
+    fireEvent.click(button4);
+    fireEvent.click(button5);
+    expect(runningTotal.textContent).toEqual("12345");
+  });
+
+  it("should change the running total to 25", () => {
+    const button5 = container.getByTestId("number5");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button5);
+    const addOperator = container.getByTestId("operator-add");
+    fireEvent.click(addOperator);
+    fireEvent.click(addOperator);
+    fireEvent.click(addOperator);
+    fireEvent.click(addOperator);
+    expect(runningTotal.textContent).toEqual("25");
+  });
+
+  it("should change the running total to 0", () => {
+    const button5 = container.getByTestId("number5");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button5);
+    const addOperator = container.getByTestId("operator-add");
+    const button3 = container.getByTestId("number3");
+    fireEvent.click(button3);
+    fireEvent.click(addOperator);
+    const equalsOperator = container.getByTestId("operator-equals");
+    fireEvent.click(equalsOperator);
+    const clearClick = container.getByTestId("clear");
+    fireEvent.click(addOperator);
+    fireEvent.click(addOperator);
+    fireEvent.click(addOperator);
+    fireEvent.click(addOperator);
+    fireEvent.click(clearClick);
+    expect(runningTotal.textContent).toEqual("0");
+  });
 });
