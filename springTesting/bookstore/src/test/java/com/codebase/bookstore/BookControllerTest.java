e115675454e973cfaa7706eeabe73ff0076cdaa1
George Tegos <george.tegos.22@gmail.com>
2023-10-27 20:10:07 +0100

    React Testing subtract

--- springTesting/bookstore/src/test/java/com/codebase/bookstore/BookControllerTest.java
+++ springTesting/bookstore/src/test/java/com/codebase/bookstore/BookControllerTest.java
@@ -1,6 +1,5 @@
 package com.codebase.bookstore;
 
-
 import com.codebase.bookstore.models.Book;
 import com.codebase.bookstore.repositories.BookRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,61 +33,53 @@ public class BookControllerTest {
     @MockBean
     private BookRepository bookRepository;
 
-
     @Test
-    public void testGetAllBooksShouldReturn200OK() throws Exception{
-//        assumption
+    public void testGetAllBooks_ShouldReturn200OK() throws Exception {
         List<Book> books = Arrays.asList(new Book("Book1", "Author1"), new Book("Book2", "Author2"));
         given(bookRepository.findAll()).willReturn(books);
 
-//                         Call the tested method
         mockMvc.perform(get("/books")
-                .accept(MediaType.APPLICATION_JSON))
-//                Validations/Expectations
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$[*].id").isNotEmpty());
     }
 
     @Test
-    public void testGetValidBookShouldReturn200OK() throws Exception{
-//        assumption
-        Book expectedBook = new Book("Book 1", "Author 1");
+    public void testGetValidBook_ShouldReturn200OK() throws Exception {
+        Book expectedBook = new Book("Book1", "Author1");
         expectedBook.setId(1L);
         given(bookRepository.findById(1L)).willReturn(Optional.of(expectedBook));
 
-//                         Call the tested method
         mockMvc.perform(get("/books/1")
-                .accept(MediaType.APPLICATION_JSON))
-//                Validations / expectations
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id").value(1L))
-                .andExpect(jsonPath("$.title").value("Book 1"))
-                .andExpect(jsonPath("$.author").value("Author 1"));
+                .andExpect(jsonPath("$.title").value("Book1"))
+                .andExpect(jsonPath("$.author").value("Author1"));
+
     }
 
     @Test
-    public void testGetNonExistenBook_Return404NotFound() throws Exception{
-        mockMvc.perform(get("/book/9999"))
+    public void testGetNonExistentBook_Returns404NotFound() throws Exception {
+        mockMvc.perform(get("/books/99999"))
                 .andExpect(status().isNotFound());
     }
 
     @Test
-    public void testCreateBook_ShouldReturn201Created() throws Exception{
-        Book book = new Book("New Book","New Author");
-//          Assumption
-        given(bookRepository.save(Mockito.any(Book.class)))
-                .willReturn(book);
+    public void testCreateBook_ShouldReturn201Created() throws Exception {
+        Book newBook = new Book("NewBook", "NewAuthor");
+        given(bookRepository.save(Mockito.any(Book.class))).willReturn(newBook);
 
-//                  Call the tested Method
         mockMvc.perform(post("/books")
-                     .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(book)))
-//                Validations / Expectations
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newBook)))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.title").value("New Book"))
-                .andExpect(jsonPath("$.author").value("New Author"));
+                .andExpect(jsonPath("$.title").value("NewBook"))
+                .andExpect(jsonPath("$.author").value("NewAuthor"));
+
     }
 }
--- week_16/day_1/2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
+++ week_16/day_1/2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import Calculator from '../containers/Calculator';
-import {render, fireEvent} from '@testing-library/react';
+import React from "react";
+import Calculator from "../containers/Calculator";
+import { render, fireEvent } from "@testing-library/react";
+import KeyPad from "../components/KeyPad";
 
-describe('Calculator', () => {
+describe("Calculator", () => {
   let container;
   beforeEach(() => {
-    container = render(<Calculator/>)
-  })
+    container = render(<Calculator />);
+  });
 
-  it('should change running total on number enter', () => {
-    const button4 = container.getByTestId('number4');
-    const runningTotal = container.getByTestId('running-total');
+  it("should change running total on number enter", () => {
+    const button4 = container.getByTestId("number4");
+    const runningTotal = container.getByTestId("running-total");
     fireEvent.click(button4);
-    expect(runningTotal.textContent).toEqual('4');
-  })
-})
+    expect(runningTotal.textContent).toEqual("4");
+  });
 
+  it("should change the running total to 5", () => {
+    const button4 = container.getByTestId("number4");
+    const button1 = container.getByTestId("number1");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button4);
+    const addOperator = container.getByTestId("operator-add");
+    const equalsOperator = container.getByTestId("operator-equals");
+    fireEvent.click(addOperator);
+    fireEvent.click(button1);
+    fireEvent.click(equalsOperator);
+    expect(runningTotal.textContent).toEqual("5");
+  });
+
+  it("should change the running total to 3", () => {
+    const button7 = container.getByTestId("number7");
+    const button4 = container.getByTestId("number4");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button7);
+    const subtractOperator = container.getByTestId("operator-subtract");
+    const equalsOperator = container.getByTestId("operator-equals");
+    fireEvent.click(subtractOperator);
+    fireEvent.click(button4);
+    fireEvent.click(equalsOperator);
+    expect(runningTotal.textContent).toEqual("3");
+  });
+});
