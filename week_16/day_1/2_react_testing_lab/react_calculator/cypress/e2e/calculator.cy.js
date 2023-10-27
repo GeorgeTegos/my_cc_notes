97f4cfeeea7da903b36f960dc48d95026af42963
George Tegos <george.tegos.22@gmail.com>
2023-10-27 20:34:36 +0100

    integration test 2

--- week_16/day_1/2_react_testing_lab/react_calculator/cypress/e2e/calculator.cy.js
+++ week_16/day_1/2_react_testing_lab/react_calculator/cypress/e2e/calculator.cy.js
@@ -1,10 +1,18 @@
 describe("Calculator", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
-  })
+  });
 
-  it('should have working number buttons', () => {
-    cy.get('#number2').click();
-    cy.get('.display').should('contain', '2')
-  })
-})
+  it("should have working number buttons", () => {
+    cy.get("#number2").click();
+    cy.get(".display").should("contain", "2");
+  });
+
+  it("should operators update the display", () => {
+    cy.get("#number2").click();
+    cy.get("#operator_add").click();
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "4");
+  });
+});
